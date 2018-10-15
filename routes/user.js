c89972db1d77459c69307d232ced09c4a490ac4a
张张涛 <zhangzhangtao@zhangzhangtaodeMacBook-Pro.local>
2018-10-15 15:53:23 +0800

    cccc

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var jwt = require('jsonwebtoken');
 var router = express.Router();
+var formidable = require('formidable');
+
+// Import mongodb dependency
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+const ObjectID = mongodb.ObjectID
+const url = 'mongodb://101.132.46.146:27017'
+const dbName = 'meetingtea'
 
 function qx(){
 
@@ -11,8 +19,59 @@ router.get('/', (req, res, next) => {
   next()
 })
 
-router.get('/addusers', (req, res, next) => {
-  
+
+router.get('/getUsers', (req, res, next) => {
+  MongoClient.connect(url, (err, client) => {
+	  const db = client.db(dbName)
+    const collection = db.collection('user')
+    collection.find({}).toArray((err, docs) => {
+			res.send(docs)
+	    client.close()
+    });
+	});
+});
+
+router.post('/touxiang', (req, res, next) => {
+  var form = new formidable.IncomingForm();   //创建上传表单
+  form.encoding = 'utf-8';        //设置编辑
+  form.uploadDir = 'public/images/upload';     //设置上传目录
+  form.keepExtensions = true;     //保留后缀
+  form.maxFieldsSize = 2 * 1024 * 1024;   //文件大小
+
+  form.parse(req, function(err, fields, files) {
+    if (err) {
+      res.locals.error = err;
+      res.send({err:'err'})
+      return;
+    }
+    var extName = 'png';  //后缀名
+    switch (files.file.type) {
+      case 'image/pjpeg':
+        extName = 'jpg';
+        break;
+      case 'image/jpeg':
+        extName = 'jpg';
+        break;
+      case 'image/png':
+        extName = 'png';
+        break;
+      case 'image/x-png':
+        extName = 'png';
+        break;
+    }
+
+    if(extName.length == 0){
+      res.locals.error = '只支持png和jpg格式图片';
+      res.send('error', '只支持png和jpg格式图片');
+      return;
+    }
+    var headPortraitPath = files.file.path;
+
+    res.send({
+      success:true,
+      headPortraitPath:headPortraitPath
+    });
+  });
 });
 
 router.post('/login', (req, res, next) => {
